062e545b2ef6550822b4b66cb09ede434b39845e
Miran Mustafa <miran.mustafa@outlook.com>
2018-05-25 23:53:33 +0200

    blockHash method and some tests

--- dev/test.js
+++ dev/test.js
@@ -1,22 +1,25 @@
 const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
+const previousBlockHash = '9FH498HF9C34H93H4J9H3948H3HOIJ02';
+const currentBlockData = [
+    {
+        amount:10,
+        sender:'9283283NJKDKFJ0239KLDFJ',
+        recipient: 'DSDIJFI094N893409U4H'
+    },
+    {
+        amount:20,
+        sender:'JK8KI29DJ49FJ0239KLDFJ',
+        recipient: 'DSDIJFI0JE92JD94N893409U4H'
+    },
+    {
+        amount:30,
+        sender:'9KJDIWK93KDKFJ0239KLDFJ',
+        recipient: '923JKSDIA94N893409U4H'
+    },
+];
+const nonce = 100;
 
-bitcoin.createNewBlock(123123,'ASD12ASD','1SSDFSDF123');
-
-bitcoin.createNewTransaction(10,'ALEXASDASD9Q','JENQFN230OW918');
-
-bitcoin.createNewBlock(8273,'ASDKDJ12ASD','9SDFAKPQEF123');
-
-bitcoin.createNewTransaction(99,'ALEXASDASD9Q','JENQFN230OW918');
-
-bitcoin.createNewTransaction(33,'ALEXASDASD9Q','JENQFN230OW918');
-
-bitcoin.createNewTransaction(1,'ALEXASDASD9Q','JENQFN230OW918');
-
-bitcoin.createNewBlock(1111,'09AOQWM12AS','92ASNKD01923');
-
-
-
-
-console.log(bitcoin.chain[2]);
+const a =bitcoin.hashBlock(previousBlockHash,currentBlockData,nonce);
+console.log(a);
