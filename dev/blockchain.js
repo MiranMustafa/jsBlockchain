d958b76a1a052d73fbc0381a109e63accb4a8cdd
Miran Mustafa <miran.mustafa@outlook.com>
2018-06-06 21:00:46 +0200

    Express js

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -3,6 +3,7 @@ const sha256 = require('sha256');
 function Blockchain(){
     this.chain = [];
     this.pendingTransactions= [];
+    this.createNewBlock(1, '0' , '0');
 }
 
 Blockchain.prototype.createNewBlock = function(nonce,previousBlockHash,hash){
@@ -48,4 +49,5 @@ Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData)
     return nonce;
 
 }
+
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,26 +1,5 @@
 const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
-const previousBlockHash = '9FH498HF9C34H93H4J9H3948H3HOIJ02';
-const currentBlockData = [
-    {
-        amount:10,
-        sender:'9283283NJKDKFJ0239KLDFJ',
-        recipient: 'DSDIJFI094N893409U4H'
-    },
-    {
-        amount:20,
-        sender:'JKKI29DJ49FJ0239KLDFJ',
-        recipient: 'DSDJFI0JE92JD94N893409U4H'
-    },
-    {
-        amount:30,
-        sender:'9KJDIWK93KDKFJ0239KLFJ',
-        recipient: '923JKSDIA94N893409U4H'
-    },
-];
-let a =bitcoin.proofOfWork(previousBlockHash,currentBlockData);
-console.log(a);
 
-let hash = bitcoin.hashBlock(previousBlockHash,currentBlockData,a);
-console.log(hash);
+console.log(bitcoin);
